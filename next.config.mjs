77257f399bd560739269013dc80efb3095f4a72a
IsaacDaoustSecE <n01726475@humber.ca>
2025-04-21 15:22:20 -0400

    Implement Pages deployment

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+    output: "export",
+    // Replace with your repository name
+    basePath: process.env.NODE_ENV === "production" ? "/WellnessCenter" : "",
+    assetPrefix:
+        process.env.NODE_ENV === "production" ? "/WellnessCenter/" : "",
+    images: {
+        unoptimized: true,
+    },
+};
 
 export default nextConfig;
