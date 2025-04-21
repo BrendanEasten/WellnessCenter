cb9f2e9e05943a4feafdab153881d8eefe2617cb
IsaacDaoustSecE <n01726475@humber.ca>
2025-04-21 15:27:12 -0400

    Make API route static for testing pages

--- src/app/api/motivation/route.js
+++ src/app/api/motivation/route.js
@@ -1,13 +1,18 @@
-export async function GET() {
-  try {
-    const res = await fetch('https://zenquotes.io/api/random');
-    if (!res.ok) throw new Error('External API error');
+export const dynamic = "force-static";
 
-    const data = await res.json();
-    return Response.json(data);
-  } catch (error) {
-    return new Response(JSON.stringify({ error: 'Failed to fetch quote' }), {
-      status: 500,
-    });
-  }
+export async function GET() {
+    try {
+        const res = await fetch("https://zenquotes.io/api/random");
+        if (!res.ok) throw new Error("External API error");
+
+        const data = await res.json();
+        return Response.json(data);
+    } catch (error) {
+        return new Response(
+            JSON.stringify({ error: "Failed to fetch quote" }),
+            {
+                status: 500,
+            }
+        );
+    }
 }
